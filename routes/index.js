3512bb6b0ed64ee2fb0d328e35ad6ae861b7125e
Yahav Amsalem <yaamsale@microsoft.com>
2018-03-03 21:43:25 +0200

    Added mongo id verification.

--- routes/index.js
+++ routes/index.js
@@ -20,9 +20,38 @@ router.get('/:id', function(req, res, next) {
     const MongoClient = require('mongodb').MongoClient;
     const url = process.env.MONGO_URL;
 
+    // Database Name
+    //const dbName = 'myproject';
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function(err, client) {
+        if (err) {
+            console.log("Error on mongo connection: " + err.message)
+        } else {
+            console.log("Connected correctly to server");
+
+            const db = client.db('guests');
+
+            // Insert a single document
+            db.collection('guests').findOne({"id": id}, function (err, guest_doc) {
+                if (err) {
+                    console.log("Error getting ID " + id + " message:" + err.message)
+                } else {
+                    if (guest_doc) {
+                        // Found ID - OK!
+                        console.log(guest_doc)
+                        res.render('index', {'id': id});
+                    } else {
+                        // ID not found - put a not found page
+                    }
+                }
+
+                client.close();
+            });
+        }
+    });
 
 
-    res.render('index', {id: req.params.id});
 });
 
 router.post('/', function(req, res, next) {
